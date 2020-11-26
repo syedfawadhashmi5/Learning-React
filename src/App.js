ade615bb0ae6c3593ef833c81bf11ed1e1273e75
syedfawadhashmi5 <59658569+syedfawadhashmi5@users.noreply.github.com>
2020-11-25 21:58:36 -0800

    learning react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+// import ClassApp from './ClassComponent/classComponent'
+import FunctionApp from './FunctionComponent/FunctionApp'
+
+function App() {
+  return (
+  <div className='container'>
+<h1 className='text'>Learning React</h1>
+
+{/* <ClassApp /> */}
+
+<FunctionApp />
+  </div>
+  );
+}
+
+export default App;
